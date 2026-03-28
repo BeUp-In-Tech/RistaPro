4d5f4ac7124f3f07a59ab7eb7e3663654eba83bf
unknown <nishanahmed13913@gmail.com>
2026-03-28 10:41:46 +0600

    Initialized RistaPro file folder

---
+++ src/app/config/env.ts
@@ -0,0 +1,70 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+interface EnvInterfaces {
+  PORT: string;
+  MONGO_URI: string;
+  NODE_ENV: 'development' | 'production';
+  JWT_SECRET: string;
+  JWT_EXPIRATION: string;
+  JWT_REFRESH_SECRET: string;
+  JWT_REFRESH_EXPIRATION: string;
+  BCRYPT_SALT_ROUND: string;
+
+  FRONTEND_URL: string;
+
+  REQUEST_RATE_LIMIT: number;
+  REQUEST_RATE_LIMIT_TIME: number;
+
+  EXPRESS_SESSION_SECRET: string;
+}
+
+const loadEnvVarbles = (): EnvInterfaces => {
+  const requireEnvVariables: string[] = [
+    'PORT',
+    'MONGO_URI',
+    'JWT_SECRET',
+    'NODE_ENV',
+
+    'JWT_SECRET',
+    'JWT_EXPIRATION',
+    'JWT_REFRESH_SECRET',
+    'JWT_REFRESH_EXPIRATION',
+    
+    'BCRYPT_SALT_ROUND',
+ 
+    'FRONTEND_URL',
+
+    'REQUEST_RATE_LIMIT',
+    'REQUEST_RATE_LIMIT_TIME',
+
+    "EXPRESS_SESSION_SECRET"
+  ];
+
+  requireEnvVariables.forEach((KEY) => {
+    if (!process.env[KEY]) {
+      throw new Error(`Missing required env variable ${KEY}`);
+    }
+  });
+
+  return {
+    MONGO_URI: process.env.MONGO_URI as string,
+    PORT: process.env.PORT as string,
+    NODE_ENV: process.env.NODE_ENV as 'development' | 'production',
+    BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
+
+    JWT_SECRET: process.env.JWT_SECRET as string,
+    JWT_EXPIRATION: process.env.JWT_EXPIRATION as string,
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+    JWT_REFRESH_EXPIRATION: process.env.JWT_REFRESH_EXPIRATION as string,
+
+    FRONTEND_URL: process.env.FRONTEND_URL as string,
+
+    REQUEST_RATE_LIMIT_TIME: Number(process.env.REQUEST_RATE_LIMIT_TIME) as number,
+    REQUEST_RATE_LIMIT:Number( process.env.REQUEST_RATE_LIMIT) as number,
+
+    EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string,
+  };
+};
+
+export default loadEnvVarbles();
