c18035c5f3858b485a4e18144b0e778097baa36a
unknown <nishanahmed13913@gmail.com>
2026-03-31 11:00:37 +0600

    created plan create, read, upudate api

--- src/app/modules/plan/plan.interface.ts
+++ src/app/modules/plan/plan.interface.ts
@@ -1,4 +1,8 @@
-export type PlanKey = 'free' | 'gold' | 'platinum';
+import { Types } from 'mongoose';
+
+export const PLAN_KEYS = ['free', 'gold', 'platinum'] as const;
+
+export type PlanKey = (typeof PLAN_KEYS)[number];
 
 export interface Plan {
   key: PlanKey;
@@ -11,4 +15,33 @@ export interface Plan {
   canVideoCall: boolean;
   profileBoost: boolean;
   featureList: string[];
+}
+
+export interface PlanAdminConfig {
+  price: number;
+  isActive: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+  createdBy: Types.ObjectId;
+  updatedBy: Types.ObjectId;
+}
+
+export interface IPlan extends Plan {
+  price: number;
+  isActive: boolean;
+  sortOrder: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+  createdBy: Types.ObjectId;
+  updatedBy: Types.ObjectId;
+}
+
+export interface CreatePlanPayload {
+  planType: PlanKey;
+  price: number;
+}
+
+export interface UpdatePlanPayload {
+  price?: number;
+  isActive?: boolean;
 }
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { authRouter } from '../modules/auth/auth.routes';
+import { planRoutes } from '../modules/plan/plan.routes';
 export const router = Router();
 
 const moduleRoutes =[
@@ -7,10 +8,13 @@ const moduleRoutes =[
     path: '/auth',
     route: authRouter,
   },
+  {
+    path: '/plans',
+    route: planRoutes,
+  },
 ];
 
 
 moduleRoutes.forEach((r) => {
   router.use(r.path, r.route);
 });
-
